57d42a9c820cf5cf69d366069262e370360a2880
Mike Puskar <puskar49@gmail.com>
2022-01-17 15:38:11 -0600

    initial

---
+++ ExampleCallouts/ExampleCallouts/ExamplePlugin.cs
@@ -0,0 +1,7 @@
+﻿[assembly: Rage.Attributes.Plugin("ExamplePlugin", Description = "An example plugin.", Author = "Immersive Plugins Team")]
+namespace ExampleCallouts
+{
+    public class ExamplePlugin
+    {
+    }
+}
